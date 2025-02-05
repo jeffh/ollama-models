ac4bb847e54041d2bd98cfb0eb426c72ba17c124
Jeff Hui <jeff@jeffhui.net>
2025-02-05 09:27:34 -0800

    add e5-small

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -xe
 
 ./convert.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0
 
 ./generate-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0
 
 ./push-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0

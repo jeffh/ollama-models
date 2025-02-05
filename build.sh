a17556543c9868dbb1ef0cb6fa7cb6b6f31cba62
Jeff Hui <jeff@jeffhui.net>
2025-02-05 09:27:28 -0800

    experiment with another model

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 set -xe
 
-./convert.sh intfloat/multilingual-e5-large:f32 intfloat/multilingual-e5-large:f16 intfloat/multilingual-e5-large:q8_0 \
-    intfloat/multilingual-e5-large-instruct:f32 intfloat/multilingual-e5-large-instruct:f16 intfloat/multilingual-e5-large-instruct:q8_0
+./convert.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
+    intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
+    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
 
 ./generate-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
-    intfloat/multilingual-e5-large-instruct:f32,f16,q8_0
+    intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
+    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
 
 ./push-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
-    intfloat/multilingual-e5-large-instruct:f32,f16,q8_0
+    intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
+    sentence-transformers/all-MiniLM-L6-v2:f32,f16,q8_0
--- converter/download.sh
+++ converter/download.sh
@@ -19,16 +19,19 @@ do
     MODEL_NAME=`echo "$HF_MODEL" | awk -F '/' '{print $2}'`
     echo "Downloading ${HF_MODEL} as ${MODEL_NAME}..."
 
-    # format can be one of: f32,f16,bf16,q8_0,tq1_0,tq2_0,auto
-    # but auto may produce bf16, which is not common among all hardware
-    FORMAT=`echo "$ARG" | awk -F ':' '{print $2}'`
-    if [[ -z "${FORMAT}" ]]; then
-        FORMAT=f32
-    fi
-
     DL_DIR="hf-models/`echo "${HF_MODEL}" | sed 's/\//--/g'`"
     if [[ ! -e "${DL_DIR}" ]]; then
         huggingface-cli download "${HF_MODEL}" --local-dir "${DL_DIR}" || error "failed to download model: $HF_MODEL"
     fi
-    python llama.cpp/convert_hf_to_gguf.py --outtype "${FORMAT}" --model-name "${MODEL_NAME}" --outfile /models "${DL_DIR}" || error "failed to convert model ${HF_MODEL}"
+
+    # format can be one of: f32,f16,bf16,q8_0,tq1_0,tq2_0,auto
+    # but auto may produce bf16, which is not common among all hardware
+    FORMATS=`echo "$ARG" | awk -F ':' '{print $2}'`
+    if [[ -z "${FORMAT}" ]]; then
+        FORMAT=f32
+    fi
+    IFS="," read -ra FORMAT <<< "$FORMATS"
+    for format in "${FORMAT[@]}"; do
+        python llama.cpp/convert_hf_to_gguf.py --outtype "${format}" --model-name "${MODEL_NAME}" --outfile /models "${DL_DIR}" || error "failed to convert model ${HF_MODEL}"
+    done
 done

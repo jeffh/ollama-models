693820cb226aea5cd16361e1cdc8b6ce61a7b3a3
Jeff Hui <jeff@jeffhui.net>
2025-02-05 09:46:48 -0800

    add e5-base-v2

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ set -xe
 
 ./convert.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    intfloat/multilingual-e5-small:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0 \
+    intfloat/e5-base-v2:f32,f16,q8_0
 
 ./generate-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    intfloat/multilingual-e5-small:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0 \
+    intfloat/e5-base-v2:f32,f16,q8_0
 
 ./push-ollamas.sh intfloat/multilingual-e5-large:f32,f16,q8_0 \
     intfloat/multilingual-e5-large-instruct:f32,f16,q8_0 \
-    intfloat/multilingual-e5-small:f32,f16,q8_0
+    intfloat/multilingual-e5-small:f32,f16,q8_0 \
+    intfloat/e5-base-v2:f32,f16,q8_0
